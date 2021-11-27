665ad166f0cd6138b2c3361a7fae6ff105395f2d
Jérémy Archier <89311027+Jershred@users.noreply.github.com>
2021-11-27 18:36:50 +0100

    Add files via upload

--- Python code/elements_finis_unidimensionnels_lineaire_non_adiabatique.py
+++ Python code/elements_finis_unidimensionnels_lineaire_non_adiabatique.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov  2 16:36:45 2021
+Created on Tue Nov 23 16:57:40 2021
 
 @author: Jershred
 """
@@ -28,6 +28,7 @@ H=5e-3
 hi=1e-3
 
 h=100
+h1=80
 P=2*np.pi*r
 Lambda=40
 S=np.pi*r**2
@@ -43,22 +44,23 @@ M=m*hi/6*np.array([[2,1],[1,2]])
 A=sousblock(6,K+M)#Matrice global
 A[0,0]=1
 A[0,1]=0
+A[5,5]+=h1/Lambda
 
-B=np.array([[100],[hi*m*Tinf],[hi*m*Tinf],[hi*m*Tinf],[hi*m*Tinf],[1/2*hi*m*Tinf]])
+B=np.array([[100],[hi*m*Tinf],[hi*m*Tinf],[hi*m*Tinf],[hi*m*Tinf],[1/2*hi*m*Tinf+Tinf*h1/ Lambda]])
 
 #Résolution
 T=np.linalg.inv(A).dot(B)
 
 def Tex(x):
     m=np.sqrt(h*P/(Lambda*S))
-    y=Tinf+(T0-Tinf)*np.cosh(m*(H-x))/np.cosh(m*H)
+    y=Tinf+(T0-Tinf)*(np.cosh(m*(H-x))+h1/(Lambda*m)*np.sinh(m*(H-x)))/(np.cosh(m*H)+h1/(Lambda*m)*np.sinh(m*(H)))
     return(y)
 
 #Tracé de la température
 x=np.array([[0*hi],[1*hi],[2*hi],[3*hi],[4*hi],[5*hi]])
 plt.plot(x,T)
 plt.plot(x,Tex(x))
-plt.title('Problème de conduction convection avec condition adiabatique')
+plt.title('Problème de conduction convection avec condition non adiabatique')
 plt.xlabel('x')
 plt.ylabel('T(x)')
 plt.grid(True,which="both", linestyle='--')
@@ -75,7 +77,3 @@ plt.ylabel('Eps(x)')
 plt.xscale('log')
 plt.grid(True,which="both", linestyle='--')
 plt.show() # affiche la figure a l'ecran
-
-
-
-
